f384bb751337cd13e9bc97e8273a592e211e1df9
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2022-05-20 22:58:30 -0300

    refactor: refatora contexto e provider para que as alterações dos estado se fechem ao escopo

--- client/src/contexts/ToDoList.context.ts
+++ client/src/contexts/ToDoList.context.ts
@@ -1,15 +1,26 @@
-import { createContext, Dispatch, SetStateAction } from 'react';
+/* eslint-disable no-unused-vars */
+import { createContext } from 'react';
 import { ITask } from '../types/Task.interface';
-import { IUserTasks } from '../types/UserTasks.interface';
+import { IUser } from '../types/User.interface';
 
-interface ToDoListContextProps extends IUserTasks {
-  setUserTasks: Dispatch<SetStateAction<IUserTasks>>;
+interface ToDoListContextProps {
+  user: IUser;
+  tasks: ITask[];
+  orderedTasks: ITask[]
+  addUser(newUser: IUser): void;
+  addTask(task: ITask): void;
+  editTask(taskId: string, editingTask: ITask): void;
+  deleteTask(taskId: string): void;
+  sortTasks(ordainedTasks: ITask[]): void;
 }
 
 export const INITIAL_STATE = {
-  _id: '',
-  name: '',
+  user: {
+    _id: '',
+    name: '',
+  },
   tasks: [] as ITask[],
+  orderedTasks: [] as ITask[],
 } as ToDoListContextProps;
 
 export const ToDoListContext = createContext<ToDoListContextProps>(INITIAL_STATE);
